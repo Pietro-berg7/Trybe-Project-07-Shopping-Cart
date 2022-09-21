f762cf12fb70eb7947b610d6e869ad27575b0299
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-20 21:30:18 -0300

    Edita função fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
+const fetchProducts = async () => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${value}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
