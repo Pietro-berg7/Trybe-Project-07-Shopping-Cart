ea70962efb83b4cb85cf5cabd877963d6595d301
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-21 00:57:06 -0300

    Requisito 6 concluído

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,12 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    expect(saveCartItems('MLB1653264294')).toHaveBeenCalled(localStorage.setItem);
+  });
+  
+  it("Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems", () => {
+    saveCartItems('MLB1653264294')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItem', 'MLB1653264294')
+  });
 });
